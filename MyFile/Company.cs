e270be5a0b5c6bc0c42265b58dde5ada0b51ebc4
NastyaZh <zhovtanska20000303@gmail.com>
2019-04-08 00:45:37 +0300

    create classes to write in file

---
+++ MyFile/Company.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyFile
+{
+	class Company : IFormattable
+	{
+		string Name { set; get; }
+		string Adress { set; get; }
+		public Company(string _Name = "", string _Adress = "")
+		{
+			Name = _Name;
+			Adress = _Adress;
+		}
+		public Company(string ALL)
+		{
+			var Elems = ALL.Replace(" ", String.Empty).Split(',');
+			Name = Elems[0];
+			Adress = Elems[1];
+		}
+		public override string ToString()
+		{
+			string Result = Name + ", " + Adress;
+			return Result;
+		}
+		public string ToString(string format, IFormatProvider formatProvider)
+		{
+			return this.ToString();
+		}
+	}
+}
---
+++ MyFile/Worker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyFile
+{
+	class Worker : IFormattable
+	{
+		string FirstName { set; get; }
+		string LastName { set; get; }
+		int Money { set; get; }
+		public Worker(string _FirstName = "", string _LastName = "", int _Money = 3200)
+		{
+			FirstName = _FirstName;
+			LastName = _LastName;
+			Money = _Money;
+		}
+		public Worker(string ALL)
+		{
+			var Elems = ALL.Replace(" ", String.Empty).Split(',');
+			FirstName = Elems[0];
+			LastName = Elems[1];
+			Money = Convert.ToInt32(Elems[2]);
+		}
+		public override string ToString()
+		{
+			string Result = FirstName + ", " + LastName + ", " + Convert.ToString(Money);
+			return Result;
+		}
+
+		public string ToString(string format, IFormatProvider formatProvider)
+		{
+			return this.ToString();
+		}
+	}
+}
